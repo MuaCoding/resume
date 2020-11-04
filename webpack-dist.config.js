47451ca1dbad12b852c7dd22b000511a26792a88
XiaoMingLi <1165406082@qq.com>
2020-11-05 02:11:07 +0800

    update:url

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -54,7 +54,10 @@ module.exports = {
     new OptimizeCSSAssetsPlugin(),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      //   fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'http://xiaomingli.github.io/resume');
+      fs.writeFileSync(
+        path.resolve(outputPath, "CNAME"),
+        "http://newwhatyouneed.com/resume"
+      );
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
@@ -62,7 +65,7 @@ module.exports = {
         "--headless",
         "--disable-gpu",
         `--print-to-pdf=${path.resolve(outputPath, "resume.pdf")}`,
-        "http://xiaomingli.github.io/resume", // 这里注意改成你的在线简历的网站
+        "http://newwhatyouneed.com/resume", // 这里注意改成你的在线简历的网站
       ]);
     }),
   ],
