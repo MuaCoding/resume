30a7392b33e85a872e6be22941898b43a94fe021
lixm <1165406082@qq.com>
2021-02-24 16:03:45 +0800

    update

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -56,7 +56,7 @@ module.exports = {
       // 自定义域名
       fs.writeFileSync(
         path.resolve(outputPath, "CNAME"),
-        "http://newwhatyouneed.com/resume"
+        "https://xiaomingli.github.io/resume"
       );
 
       // 调用 Chrome 渲染出 PDF 文件
@@ -65,7 +65,7 @@ module.exports = {
         "--headless",
         "--disable-gpu",
         `--print-to-pdf=${path.resolve(outputPath, "resume.pdf")}`,
-        "http://newwhatyouneed.com/resume", // 这里注意改成你的在线简历的网站
+        "https://xiaomingli.github.io/resume", // 这里注意改成你的在线简历的网站
       ]);
     }),
   ],
